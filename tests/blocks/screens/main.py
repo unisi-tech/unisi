9c37f8ec87384421b24ec6bbf61618e88b304667
George Dernovoy <g.dernovoy@gmail.com>
2025-01-26 03:17:45 +0700

    +lazy charts

--- tests/blocks/screens/main.py
+++ tests/blocks/screens/main.py
@@ -1,8 +1,8 @@
 from unisi import *
 from blocks.tblock import config_area, tarea
 
-name = "Main"
-order = 0
+name = "Blocks"
+order = 2
 
 table = Table('Videos', 0, headers = ['Video', 'Duration',  'Links', 'Mine'], rows = [
     ['opt_sync1_3_0.mp4', '30 seconds',  '@Refer to signal1', True],
--- tests/blocks/screens/zoo.py
+++ tests/blocks/screens/zoo.py
@@ -1,9 +1,27 @@
 from data import table
 from unisi import *
-name = 'Pandas table'
+name = 'Panda & params'
 order = 1
 
 zoo_table = Table('Zoo Table', panda = table)
 
-blocks = [Block('Csv table', [], zoo_table)]
+def get_params(button, _):
+    return Info(str(block.params))
+
+block = ParamBlock('System parameters', Button('Show server params', get_params), 
+    per_device_eval_batch_size=16,
+    num_train_epochs=10, 
+    warmup_ratio=0.1, 
+    logging_steps= (10,[1,20,1]), 
+    device = ('gpu', ['cpu', 'gpu']),
+    load_best = True)
+
+def html_handler(unit, event):
+    return Info(event)
+
+html = HTML('HTML',  '<button>Click me</button> <a href="#">Link</a> <input type="text">', html_handler)
+
+html_block = Block('Block with HTML', [], html, zoo_table)
+
+blocks = [block, html_block]
 
