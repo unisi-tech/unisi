79e32623cadfaf6ed31c006e86b4d351754df10e
George Dernovoy <g.dernovoy@gmail.com>
2026-08-12 00:49:43 +0700

    check persist_location

--- tests/persistence/blocks/shared.py
+++ tests/persistence/blocks/shared.py
@@ -1,6 +1,12 @@
 from unisi import *
-eblock = Block('Shared block',  Range('Scaling', 0, options=[0.0,1.0,0.1]),                
-        Edit('Number only', 2.5),
+ed = Edit('Number only', 2.5),
+
+def get_path(*_):
+    return Info(str(user.persist_location(ed)))
+
+eblock = Block('Shared block',  Button('Get path', get_path),
+        Range('Scaling', 0, options=[0.0,1.0,0.1]),                
+        ed,   
         Block('Embedded block', 
             Edit('Edit string', 'xyz'),
             Switch('Switch', True)),
